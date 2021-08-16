f3431e9b4f9e15bbfe8de4eb41176f24f40b0d33
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2021-08-16 09:01:04 +0200

    works

--- example/src/App2.svelte
+++ example/src/App2.svelte
@@ -3,42 +3,45 @@
   let value1 = 100;
   let value2 = 33;
   let logs = [];
-  
 </script>
 
 <!-- ------------------------------------- -->
 
-<h3>Touch 2B2</h3>
-<p>Version with two input elements, one for dragging, one for editing (different colors just to see quickly which one is displayed). Double click on the first should give focus to the second and the keyboard should come up. When loosing focus on the second, editing should be ended.</p>
+<h3>Touch 2B3</h3>
+<p>
+  Bring in some of the dragging behaviour. Hoping that nothing breaks.
+</p>
 
-<hr>
+<hr />
 
 <div class="row">
-  <NumberSpinner bind:value={value1} on:consoleLog={(e) => logs = [{timestamp:Date.now(), msg:e.detail}, ...logs]} />
+  <NumberSpinner
+    bind:value={value1}
+    on:consoleLog={(e) => (logs = [{ timestamp: Date.now(), msg: e.detail }, ...logs])}
+  />
 </div>
 
-<hr>
+<hr />
 
 <div class="row">
   <NumberSpinner bind:value={value2} />
 </div>
 
-<hr>
+<hr />
 
 <input type="text" value="Some other normal input field" />
 
 <div class="console">
   {#each logs as log, i}
+    {new Date(log.timestamp).toLocaleTimeString("de-DE")}.{(log.timestamp % 1000)
+      .toString()
+      .padStart(3, "0")} – {log.msg}<br />
 
-    {(new Date(log.timestamp)).toLocaleTimeString("de-DE")}.{(log.timestamp % 1000).toString().padStart(3, '0')} – {log.msg}<br>
-
-    {#if logs[i+1]?.timestamp < log.timestamp - 200}<br>{/if}
-    
+    {#if logs[i + 1]?.timestamp < log.timestamp - 200}<br />{/if}
   {/each}
 </div>
 
 <!-- ------------------------------------- -->
-
 <style>
   hr {
     margin: 20px 0px;
@@ -52,5 +55,4 @@
     font-size: 9px;
     line-height: 1.2em;
   }
-
 </style>
--- src/NumberSpinner.svelte
+++ src/NumberSpinner.svelte
@@ -2,29 +2,88 @@
   import { createEventDispatcher, tick } from "svelte";
   const dispatch = createEventDispatcher();
 
-  export let value = 0;
-  // export let min = -Number.MAX_VALUE;
-  // export let max = Number.MAX_VALUE;
-  // export let step = 1;
+  // set any of the props with properties of this options object
+  // changing one of the options later will not update the prop!
+  export let options = {};
+
+  export let value = options.value ?? 0;
+  export let min = options.min ?? -Number.MAX_VALUE;
+  export let max = options.max ?? Number.MAX_VALUE;
+  export let step = options.step ?? 1;
+  export let precision = options.precision ?? step;
+  export let decimals = options.decimals ?? 0;
+  export let speed = options.speed ?? 1;
+  export let horizontal = options.horizontal ?? true;
+  export let vertical = options.vertical ?? false;
+  export let circular = options.circular ?? false;
+
+  let preciseValue;
+  let visibleValue;
 
   let isTouchDevice = false;
 
-  let dragging = false;
-  let editing = false;
   let dragElement, editElement;
   let dragFocussed = false;
   let editFocussed = false;
 
+  let dragging = false;
+  let hasMoved, clickX, clickY;
+  let stepFactor = 1;
+  
+  let editing = false;
+  
+  // update all values (preciseValue, visibleValue)
+  updateValues(value);
+
   function touchstartHandler(ev) {
+    dispatch('consoleLog', ev.type);
+
     isTouchDevice = true;
-    mousedownHandler(ev);
+    dragstartHandler(ev);
   }
-  function mousedownHandler(ev) {
+  function dragstartHandler(ev) {
+    dispatch('consoleLog', ev.type);
+
     dragging = true;
     dragElement.focus();
+
+    hasMoved = 0;
+    clickX = isTouchDevice ? ev.touches[0].clientX : ev.clientX;
+    clickY = isTouchDevice ? ev.touches[0].clientY : ev.clientY;
+    dragging = true;
+    updateValues(value);
+  }
+
+  function touchmoveHandler(ev) {
+    // dispatch('consoleLog', ev.type);
+
+    isTouchDevice = true;
+    dragmoveHandler(ev);
+  }
+  function dragmoveHandler(ev) {
+    // dispatch('consoleLog', ev.type);
+
+    // ev.preventDefault();
+
+    let actX = isTouchDevice ? ev.touches[0].clientX : ev.clientX;
+    let actY = isTouchDevice ? ev.touches[0].clientY : ev.clientY;
+
+    let distX = horizontal ? actX - clickX : 0;
+    let distY = vertical ? -(actY - clickY) : 0;
+
+    let stepNum = Math.abs(distX) > Math.abs(distY) ? distX : distY;
+
+    stepValue(stepNum);
+
+    clickX = actX;
+    clickY = actY;
+
+    hasMoved++;
   }
 
   async function dblclickHandler(ev) {
+    dispatch('consoleLog', ev.type);
+
     editing = true;
     await tick();
     editElement.focus();
@@ -34,33 +93,39 @@
   }
 
   function touchendHandler(ev) {
+    dispatch('consoleLog', ev.type);
+
     mouseupHandler(ev);
   }
   function mouseupHandler(ev) {
+    dispatch('consoleLog', ev.type);
+
     dragging = false;
   }
 
   function dragFocusHandler(ev) {
+    dispatch('consoleLog', ev.type);
+
     dragFocussed = true;
   }
   function dragBlurHandler(ev) {
+    dispatch('consoleLog', ev.type);
+
     dragFocussed = false;
   }
   function editFocusHandler(ev) {
+    dispatch('consoleLog', ev.type);
+
     editFocussed = true;
   }
-
   async function editBlurHandler(ev) {
-    //console.log(ev);
+    dispatch('consoleLog', ev.type);
+
     editFocussed = false;
     editing = false;
 
     // bring focus back to the drag element if the body was clicked
     setTimeout(() => {
-      console.log(document.activeElement);
-      dispatch('consoleLog', ev.type);
-      dispatch('consoleLog', document.activeElement);
-
       if (document.activeElement === document.body || document.activeElement === editElement) {
         dragElement.focus();
       }
@@ -74,12 +139,53 @@
     // }
   }
 
-  function startEditing() {
+  function startEditing() {}
 
+  function stopEditing() {}
+
+  function stepValue(numSteps) {
+    preciseValue = preciseValue ?? parseFloat(visibleValue);
+    preciseValue += numSteps * step * stepFactor * speed;
+    updateValues(preciseValue);
   }
 
-  function stopEditing() {
+  function addToValue(increment) {
+    preciseValue = preciseValue ?? parseFloat(visibleValue);
+    preciseValue += increment * stepFactor;
+    updateValues(preciseValue);
+  }
+
+  function updateValues(val) {
+    preciseValue = parseFloat(val);
+    preciseValue = keepInRange(preciseValue);
+
+    visibleValue = Math.round(preciseValue / step) * step;
+    visibleValue = visibleValue.toFixed(decimals);
     
+    value = roundToPrecision(preciseValue);
+
+    dispatch('input', parseFloat(value));
+    dispatch('change', parseFloat(value));
+  }
+  
+  function keepInRange(val) {
+    min = parseFloat(min);
+    max = parseFloat(max);
+    if (circular) {
+      let range = max - min;
+      if (range === 0) return min;
+      let fac = val < min ? Math.ceil((min - val) / range) : 0;
+      val = ((val - min + range * fac) % range) + min;
+    } else {
+      val = Math.min(Math.max(val, min), max);
+    }
+    return val;
+  }
+
+  function roundToPrecision(val) {
+    val = Math.round(parseFloat(val) / precision) * precision;
+    let dec = precision < 1 ? Math.ceil(-Math.log10(precision)) : 0;
+    return parseFloat(val.toFixed(dec));
   }
 
 </script>
@@ -87,14 +193,16 @@
 <!-- DOM --------------------------------------------------------------->
 
 <svelte:window
+  on:mousemove={dragging ? dragmoveHandler : ""}
+  on:touchmove={dragging ? touchmoveHandler : ""}
   on:mouseup|stopPropagation={dragging ? mouseupHandler : editBlurHandler}
   on:touchend|stopPropagation={dragging ? touchendHandler : editBlurHandler}
 />
 
 <input
   type="text"
-  on:mousedown|stopPropagation={mousedownHandler}
-  on:touchstart|stopPropagation={touchstartHandler}
+  on:mousedown|stopPropagation={dragstartHandler}
+  on:touchstart|stopPropagation|preventDefault={touchstartHandler}
   on:dblclick|stopPropagation={dblclickHandler}
   on:focus={dragFocusHandler}
   on:blur={dragBlurHandler}
@@ -104,10 +212,17 @@
   bind:this={dragElement}
   bind:value
   readonly={true}
+  contenteditable={false}
   tabindex="0"
 />
 <input
   class="edit"
+  on:mouseup|stopPropagation={(ev) => {
+    console.log(ev);
+  }}
+  on:touchend|stopPropagation={(ev) => {
+    console.log(ev);
+  }}
   on:focus={editFocusHandler}
   on:blur={editBlurHandler}
   class:active={editing}
@@ -124,6 +239,7 @@
     width: 120px;
     box-sizing: border-box;
     padding: 5px;
+    text-align: right;
   }
 
   .focus {
@@ -135,9 +251,18 @@
   .drag {
     user-select: none;
     color: royalblue;
+    cursor: ew-resize;
   }
   .edit {
     color: crimson;
+    cursor: default;
+  }
+
+  /* mandatory css styles, not customizable */
+
+  /* remove text selection background in non-editing mode */
+  .drag::selection {
+    background: #0000;
   }
 
   .active {
