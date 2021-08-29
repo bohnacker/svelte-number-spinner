6fd048851458c87bcc9316e7731f156a62a838cc
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2021-08-29 09:00:45 +0200

    removed some console.log

--- src/NumberSpinner.svelte
+++ src/NumberSpinner.svelte
@@ -254,13 +254,13 @@
       if (!focussed) {
         focussed = true;
         dispatch('focus');
-        console.log("Focus");
+        // console.log("Focus");
       }
     } else {
       if (focussed) {
         focussed = false;
         dispatch('blur');
-        console.log("Blur");
+        // console.log("Blur");
       }
     }
   }
